16235cf31e44a0f87ce5677818ee8e22e0dc7489
manjuu-eater <88940383+manjuu-eater@users.noreply.github.com>
2025-03-15 20:09:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+(()=>{
+  const e=document.querySelector("div.a");
+  e.innerHTML += `window.screen.width: ${window.screen.width}<BR>`;
+  e.innerHTML += `window.screen.height: ${window.screen.height}<BR>`;
+  e.innerHTML += `screen.width:  ${screen.width}<BR>`;
+  e.innerHTML += `screen.height: ${screen.height}<BR>`;
+  e.innerHTML += `window.devicePixelRatio: ${window.devicePixelRatio}<BR>`;
+  
+	e.innerHTML += (`screen.height:                           ${screen.height}<BR>`)
+	e.innerHTML += (`screen.availHeight:                      ${screen.availHeight}<BR>`)
+	e.innerHTML += (`window.innerHeight:                      ${window.innerHeight}<BR>`)
+	e.innerHTML += (`document.documentElement.clientHeight:   ${document.documentElement.clientHeight}<BR>`)
+	e.innerHTML += (`document.documentElement.scrollHeight:   ${document.documentElement.scrollHeight}<BR>`)
+	e.innerHTML += (`window.devicePixelRatio:                 ${window.devicePixelRatio}<BR>`)
+	e.innerHTML += (`screen.height * window.devicePixelRatio: ${screen.height * window.devicePixelRatio}<BR>`)
+})();
